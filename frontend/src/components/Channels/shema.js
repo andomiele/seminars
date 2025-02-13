e532daeb7fe6edfb7f65660a2cf9da5e26dae70e
Andomiele <alina2005@ya.ru>
2025-02-13 18:54:25 +0300

    prepare .env file, correct the error text when creating a channel

--- frontend/src/components/Channels/shema.js
+++ frontend/src/components/Channels/shema.js
@@ -3,7 +3,7 @@ import * as Yup from 'yup';
 const channelSchema = (channelsList) => Yup.object({
   name: Yup.string()
     .min(3, 'symbolRange')
-    .max(20, 'max')
+    .max(20, 'symbolRange')
     .required('required')
     .notOneOf(channelsList, 'unic'),
 });
--- frontend/src/components/configs/rollbar.js
+++ frontend/src/components/configs/rollbar.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
 export const rollbarConfig = {
-  accessToken: '38279758a05646feb4b0aae8b1b477c1',
-  enabled: process.env.NODE_ENV === 'production',
-  environment: 'prod',
+  accessToken: import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN,
+  environment: 'production',
 };
