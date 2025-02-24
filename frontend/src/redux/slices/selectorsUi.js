5768b45e998895e94b9cf50c3449ed4bb5e6850a
Andomiele <alina2005@ya.ru>
2025-02-25 02:33:12 +0300

    Move errors to constants

--- frontend/src/redux/slices/selectorsUi.js
+++ frontend/src/redux/slices/selectorsUi.js
@@ -1,11 +1,14 @@
 import { createSelector } from '@reduxjs/toolkit';
+import { UNAUTHORIZED_ERROR, CONFLICT_ERROR } from './constants.js';
 
 const selectUi = (state) => state.ui;
 
 const selectError = createSelector(selectUi, ((state) => state.error));
 
+export const selectCurrentChannel = createSelector(selectUi, ((state) => state.channel));
+
 export const selectToastsError = createSelector(selectError, (error) => (
-  error.status !== 401 && error.status !== 409 ? error.status : null));
+  error.status !== UNAUTHORIZED_ERROR && error.status !== CONFLICT_ERROR ? error.status : null));
 
 export const selectAuthError = createSelector(selectError, (error) => (
-  error.status === 401 || error.status === 409 ? error.status : null));
+  error.status === UNAUTHORIZED_ERROR || error.status === CONFLICT_ERROR ? error.status : null));
