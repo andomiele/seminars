44e02c890a35d6a461a399848b29acf9b1b41184
Andomiele <alina2005@ya.ru>
2025-02-25 17:29:40 +0300

    Remove duplicate functions and move action to uiSlice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReduser from './slices/authSlice.js';
-import chennelReduser from './slices/channelsSlice.js';
 import uiReduser from './slices/uiSlice.js';
 import { channelsApi } from '../services/channelsApi.js';
 import { messagesApi } from '../services/messagesApi.js';
@@ -9,7 +8,6 @@ import { usersApi } from '../services/authApi.js';
 export default configureStore({
   reducer: {
     auth: authReduser,
-    channel: chennelReduser,
     ui: uiReduser,
 
     [channelsApi.reducerPath]: channelsApi.reducer,
