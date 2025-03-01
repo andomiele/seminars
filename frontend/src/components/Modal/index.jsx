6d59f6177ebdb6becb6c4f9008b273bfc718952e
Andomiele <alina2005@ya.ru>
2025-03-01 23:15:54 +0300

    delete the unnecessary component and rename the remaining component

--- frontend/src/components/Modal/index.jsx
+++ frontend/src/components/Modal/index.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { Modal } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { hideModalInfo } from '../../redux/slices/uiSlice';
-import getModal from './index';
+import AddChannelModal from '../Channels/AddChannelModal';
+import EditChannelModal from '../Channels/EditChannelModal';
+import DeleteChannelModal from '../Channels/DeleteChannelModal';
+
+const modals = {
+  adding: AddChannelModal,
+  removing: DeleteChannelModal,
+  editing: EditChannelModal,
+};
 
 const renderModal = ({ uiState, hideModal }) => {
   if (uiState.modal.isVisible === false) {
     return null;
   }
-  const Component = getModal(uiState.modal.type);
+  const Component = modals[uiState.modal.type];
   return (
     <>
       <div className="fade modal-backdrop show" />
