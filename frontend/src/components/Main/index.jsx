b36f6c5b2b326585280f4d09cc132d057aa2d8a5
Andomiele <alina2005@ya.ru>
2025-03-03 16:54:16 +0300

    remove unnecessary authorization checks

--- frontend/src/components/Main/index.jsx
+++ frontend/src/components/Main/index.jsx
@@ -1,29 +1,19 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import { useNavigate } from 'react-router';
-import { useSelector } from 'react-redux';
 import Channels from '../Channels/index.jsx';
 import Chat from '../Chat/index.jsx';
-import { selectIsAuth } from '../../redux/slices/authSlice.js';
 
-const MainPage = () => {
-  const navigate = useNavigate();
-  const isAuth = useSelector(selectIsAuth);
-
-  return (
-    !isAuth ? navigate('/login') : (
-      <Container className="container h-100 my-4 overflow-hidden rounded shadow">
-        <Row className="row h-100 bg-white flex-md-row">
-          <Col className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
-            <Channels />
-          </Col>
-          <Col className="col p-0 h-100">
-            <Chat />
-          </Col>
-        </Row>
-      </Container>
-    )
-  );
-};
+const MainPage = () => (
+  <Container className="container h-100 my-4 overflow-hidden rounded shadow">
+    <Row className="row h-100 bg-white flex-md-row">
+      <Col className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
+        <Channels />
+      </Col>
+      <Col className="col p-0 h-100">
+        <Chat />
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default MainPage;
